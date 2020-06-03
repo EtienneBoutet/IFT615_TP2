341fe4509db984545fabb9ab16d8027471ec5ffd
Etienne <etienne.boutet@outlook.com>
2020-06-03 16:03:25 -0400

    add solution for question_2

--- question_2/solution_sudoku.py
+++ question_2/solution_sudoku.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #####
-# VotreNom (VotreMatricule) .~= À MODIFIER =~.
+# Étienne Boutet - boue2327
+# Raphael Valois - valr2802
 ###
 
 from pdb import set_trace as dbg  # Utiliser dbg() pour faire un break dans votre code.
@@ -22,9 +23,16 @@ import numpy as np
 #
 # retour: Un tuple contenant un booléen indiquant si il y a eu des changements et le csp.
 ###
+def contrainte(Xi, Xj):
+    return Xi != Xj
+
 def reviser(Xi, Xj, csp):
-    #TODO: .~= À COMPLÉTER =~.
-    return False, csp
+    revise = False
+    for x in csp.domaines[Xi]:
+        if not any([x != y for y in csp.domaines[Xj]]):
+            csp.domaines[Xi].remove(x)
+            revise = True
+    return revise, csp
 
 
 #####
@@ -37,9 +45,20 @@ def reviser(Xi, Xj, csp):
 # retour: Un tuple contenant le csp optimisé et un booléen indiquant si aucune contrainte n'est violée.
 ###
 def AC3(csp):
-    #TODO: .~= À COMPLÉTER =~.
-    return csp, True
+    file_arcs = csp.arcs()
 
+    while file_arcs:
+        Xi, Xj = file_arcs.pop()
+        revise, csp = reviser(Xi, Xj, csp)
+        if revise:
+            if len(csp.domaines[Xi]) == 0:
+                return csp, False
+            else:
+                for Xk in csp.contraintes[Xi]:
+                    if Xk != Xj:
+                        file_arcs.append((Xk, Xi))
+
+    return csp, True
 
 #####
 # est_compatible: Fonction vérifiant la légalité d'une affectation.
@@ -57,7 +76,9 @@ def AC3(csp):
 # retour: Un booléean indiquant si l'affectation de la valeur v à la case X est légale.
 ###
 def est_compatible(X, v, assignations, csp):
-    #TODO: .~= À COMPLÉTER =~.
+    for cle, valeur in assignations.items():
+        if v == valeur and cle in csp.contraintes[X]:
+            return False
     return True
 
 
@@ -74,8 +95,45 @@ def est_compatible(X, v, assignations, csp):
 # retour: Le dictionnaire des assignations (case => valeur)
 ###
 def backtrack(assignations, csp):
-    #TODO: .~= À COMPLÉTER =~.
-    return assignations
+    if len(assignations) == len(csp.variables):
+        return assignations
+
+    X = var_non_assignee(assignations, csp)
+
+    for v in valeurs_ordonnees(X, csp):
+        if est_compatible(X, v, assignations, csp):
+            assignations[X] = v
+            csp.domaines[X] = [v]
+            csp_prime, ok = AC3(csp.copy())
+            if ok:
+                resultat = backtrack(assignations, csp_prime)
+                if resultat:
+                    return resultat
+            del assignations[X]
+
+    return False
+
+
+def var_non_assignee(assignations, csp):
+    non_assignees = [v for v in csp.variables if v not in assignations]
+    return min(non_assignees, key=lambda var: len(csp.domaines[var]))
+
+
+def valeurs_ordonnees(variable, csp):
+    if len(csp.domaines[variable]) == 1:
+        return csp.domaines[variable]
+    else:
+        return sorted(csp.domaines[variable], key=lambda valeur: nombres_conflits(csp, variable, valeur))
+
+
+def nombres_conflits(csp, variable, valeur):
+    nombre = 0
+
+    for voisin in csp.contraintes[variable]:
+        if len(csp.domaines[voisin]) > 1 and valeur in csp.domaines[voisin]:
+            nombre += 1
+
+    return nombre
 
 
 #####
@@ -90,5 +148,4 @@ def backtrack(assignations, csp):
 # retour: Le dictionnaire des assignations (case => valeur)
 ###
 def backtracking_search(csp):
-    #TODO: .~= À COMPLÉTER =~.
     return backtrack({}, csp)
